e389133523413a032930d619966d26ae8878e2f9
skillsmetrix Technologies <161203473+SkillsMetrix@users.noreply.github.com>
2024-03-08 05:22:29 +0530

    Update data.js

--- data.js
+++ data.js
@@ -1,85 +1,26 @@
-var weather = require("./weather.js");
-var location = require("./location.js");
-var argv = require("yargs")
-  .option("location", {
-    alias: "l",
-    demand: false,
-    description: "Location to fetch the weather for",
-    type: "string",
-  })
-  .help("help").argv;
 
-if (typeof argv.l === "string" && argv.l.length > 0) {
-  weather(argv.l)
-    .then(function (weather) {
-      console.log(weather);
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
-} else {
-  console.log("Location not provided...!");
-  location()
-    .then(function (loc) {
-      return weather(loc.city);
-    })
-    .then(function (currentweather) {
-      console.log(currentweather);
-    })
-    .catch(function (err) {
-      console.log(err);
-    });
-}
-----
+const express= require('express')
+const bp= require('body-parser')
+var app=express()
+app.use(express.static('public'))
+app.use(bp.json())
+uid=1
+userdata=[
+   /*  {uname:'admin',email:'admin@mail.com'},
+    {uname:'manager',email:'manager@mail.com'},
+    {uname:'QA',email:'QA@mail.com'} */
+]
+app.get('/loadusers',(req,res)=>{
+    res.send(userdata)
+})
+app.post('/adduser',(req,res)=>{
+ const data= req.body
+ data.id=uid++
+ userdata.push(data)
+ res.send(data)
+})
 
 
-  var request = require("request");
-
-module.exports = function (location) {
-  return new Promise(function (resolve, reject) {
-    var ec = encodeURIComponent(location);
-    var URL = `https://api.openweathermap.org/data/2.5/weather?q=${ec}&appid=b3aaa0b3323c0baab93aff38f75b44cb&units=metric`;
-
-    if (!location) {
-      return reject("No Location Provided");
-    }
-    request(
-      {
-        url: URL,
-        json: true,
-      },
-      function (error, response, body) {
-        if (error) {
-          reject("Unable to Fetch Data");
-        } else {
-          resolve("Its " + body.main.temp + " in " + body.name);
-        }
-      }
-    );
-  });
-};
-
-
-
-----
-  const request = require("request");
-
-var URL = "https://ipinfo.io/";
-
-module.exports = function () {
-  return new Promise(function (resolve, reject) {
-    request(
-      {
-        url: URL,
-        json: true,
-      },
-      function (error, response, body) {
-        if (error) {
-          reject("unable to detect");
-        } else {
-          resolve(body);
-        }
-      }
-    );
-  });
-};
+app.listen(4000,()=>{
+    console.log('server is ready...!');
+})
