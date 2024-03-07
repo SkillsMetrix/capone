9905d013a64d6d61d60ab31cdfe072cc2aa56e13
skillsmetrix Technologies <161203473+SkillsMetrix@users.noreply.github.com>
2024-03-07 06:34:09 +0530

    Update data.js

--- data.js
+++ data.js
@@ -1,39 +1,27 @@
-//loading the user from database
-function getUser(id) {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      console.log("Getting the user from database");
-      resolve({
-        id: id,
-        name: "Admin",
+var weather = require("./weather.js");
+var location = require("./location.js");
+var argv = require("yargs")
+  .option("location", {
+    alias: "l",
+    demand: false,
+    description: "Location to fetch the weather for",
+    type: "string",
+  })
+  .help("help").argv;
+
+if (typeof argv.l === "string" && argv.l.length > 0) {
+  weather(argv.l, function (data) {
+    console.log(data);
+  });
+} else {
+  console.log("Location not provided...!");
+  location(function (location) {
+    if (location) {
+      weather(location.city, function (data) {
+        console.log(data);
       });
-    }, 1000);
+    } else {
+      console.log("unable to guess");
+    }
   });
 }
-//loading all the blogs posted on social media
-function getBlogs(username) {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      console.log("calling RestAPI to laod the posts of user ");
-      resolve(["Post-1", "Post-2", "Post-3"]);
-    }, 1000);
-  });
-}
-// load the comments of the posts
-
-function getComments(post) {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      console.log("calling RestAPI to laod the comments " + post);
-      resolve(["Comments for " + post]);
-    }, 1000);
-  });
-}
-// print all the   (callback chain)
-
-getUser(101)
-.then(user => getBlogs(user.name))
-
-.then(blogs =>getComments(blogs[0]))
-.then(comments => console.log(comments))
-.catch(err => console.log('Error '+ err.message))
