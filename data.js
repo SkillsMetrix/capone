3cac1c6bde4126418744b5330bc61ebefa543f92
skillsmetrix Technologies <161203473+SkillsMetrix@users.noreply.github.com>
2024-03-07 07:04:24 +0530

    Update data.js

--- data.js
+++ data.js
@@ -10,70 +10,76 @@ var argv = require("yargs")
   .help("help").argv;
 
 if (typeof argv.l === "string" && argv.l.length > 0) {
-  weather(argv.l, function (data) {
-    console.log(data);
-  });
+  weather(argv.l)
+    .then(function (weather) {
+      console.log(weather);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
 } else {
   console.log("Location not provided...!");
-  location(function (location) {
-    if (location) {
-      weather(location.city, function (data) {
-        console.log(data);
-      });
-    } else {
-      console.log("unable to guess");
+  location()
+    .then(function (loc) {
+      return weather(loc.city);
+    })
+    .then(function (currentweather) {
+      console.log(currentweather);
+    })
+    .catch(function (err) {
+      console.log(err);
+    });
+}
+----
+
+
+  var request = require("request");
+
+module.exports = function (location) {
+  return new Promise(function (resolve, reject) {
+    var ec = encodeURIComponent(location);
+    var URL = `https://api.openweathermap.org/data/2.5/weather?q=${ec}&appid=b3aaa0b3323c0baab93aff38f75b44cb&units=metric`;
+
+    if (!location) {
+      return reject("No Location Provided");
     }
+    request(
+      {
+        url: URL,
+        json: true,
+      },
+      function (error, response, body) {
+        if (error) {
+          reject("Unable to Fetch Data");
+        } else {
+          resolve("Its " + body.main.temp + " in " + body.name);
+        }
+      }
+    );
   });
-}
+};
 
 
 
+----
+  const request = require("request");
 
+var URL = "https://ipinfo.io/";
 
-
-
-const request= require('request')
-
-var URL='https://ipinfo.io/'
-module.exports= function(callback) {
-
-request({
-    url:URL,
-    json:true
-},function(error,response,body){
-    if(error){
-        callback('unable to detect');
-    }else{
-        
-        callback(body);
-    }
-})
-}
-
-
-
-
-
-var request= require('request')
-
-
-module.exports= function(location,callback){
-
-    var ec= encodeURIComponent(location)
-    var URL='https://api.openweathermap.org/data/2.5/weather?q=' +ec+ '&appid=b3aaa0b3323c0baab93aff38f75b44cb&units=metric'
-
-    if(!location){
-        return callback('No Location Provided')
-    }
-request({
-    url:URL,
-    json:true
-},function(error,response,body){
-    if(error){
-        callback('Unable to Fetch Data');
-    }else {
-        callback('Its ' + body.main.temp + ' in ' +   body.name);
-    }
-
-})
-}
+module.exports = function () {
+  return new Promise(function (resolve, reject) {
+    request(
+      {
+        url: URL,
+        json: true,
+      },
+      function (error, response, body) {
+        if (error) {
+          reject("unable to detect");
+        } else {
+          resolve(body);
+        }
+      }
+    );
+  });
+};
