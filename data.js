acb31b04d7520016a20c87e26e4d4575f8a2426b
skillsmetrix Technologies <161203473+SkillsMetrix@users.noreply.github.com>
2024-03-08 05:37:21 +0530

    Update data.js

--- data.js
+++ data.js
@@ -1,26 +1,38 @@
-
-const express= require('express')
-const bp= require('body-parser')
-var app=express()
-app.use(express.static('public'))
-app.use(bp.json())
-uid=1
-userdata=[
-   /*  {uname:'admin',email:'admin@mail.com'},
+const express = require("express");
+const bp = require("body-parser");
+var app = express();
+app.use(express.static("public"));
+app.use(bp.json());
+uid = 1;
+userdata = [
+  /*  {uname:'admin',email:'admin@mail.com'},
     {uname:'manager',email:'manager@mail.com'},
     {uname:'QA',email:'QA@mail.com'} */
-]
-app.get('/loadusers',(req,res)=>{
-    res.send(userdata)
-})
-app.post('/adduser',(req,res)=>{
- const data= req.body
- data.id=uid++
- userdata.push(data)
- res.send(data)
-})
+];
+app.get("/loadusers", (req, res) => {
+  res.send(userdata);
+});
+app.get("/loaduser/:id", (req, res) => {
+  const uid = parseInt (req.params.id);
+   var mtd;
+  userdata.forEach(function (todo) {
+    if (uid === todo.id) {
+      mtd = todo;
+    }
+  });
+  if (mtd) {
+    res.json(mtd);
+  } else {
+    res.status(404).send();
+  }
+});
+app.post("/adduser", (req, res) => {
+  const data = req.body;
+  data.id = uid++;
+  userdata.push(data);
+  res.send(data);
+});
 
-
-app.listen(4000,()=>{
-    console.log('server is ready...!');
-})
+app.listen(4000, () => {
+  console.log("server is ready...!");
+});
