6c75f2dabbabe5c8d96a5fad5297952b041e318e
skillsmetrix Technologies <161203473+SkillsMetrix@users.noreply.github.com>
2024-03-08 05:50:34 +0530

    Update data.js

--- data.js
+++ data.js
@@ -1,25 +1,18 @@
 const express = require("express");
 const bp = require("body-parser");
+var _= require('underscore')
 var app = express();
 app.use(express.static("public"));
 app.use(bp.json());
 uid = 1;
-userdata = [
-  /*  {uname:'admin',email:'admin@mail.com'},
-    {uname:'manager',email:'manager@mail.com'},
-    {uname:'QA',email:'QA@mail.com'} */
-];
+userdata = [];
 app.get("/loadusers", (req, res) => {
   res.send(userdata);
 });
 app.get("/loaduser/:id", (req, res) => {
   const uid = parseInt (req.params.id);
-   var mtd;
-  userdata.forEach(function (todo) {
-    if (uid === todo.id) {
-      mtd = todo;
-    }
-  });
+   var mtd=_.findWhere(userdata,{id:uid})
+  
   if (mtd) {
     res.json(mtd);
   } else {
