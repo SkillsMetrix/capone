a67ca6b81fb8efe4a2e1122640c92f9b8766be0d
skillsmetrix Technologies <161203473+SkillsMetrix@users.noreply.github.com>
2024-03-09 07:21:57 +0530

    Update data.js

--- data.js
+++ data.js
@@ -1,55 +1,55 @@
 const express = require("express");
-var bp = require("body-parser");
-const jwt = require("jsonwebtoken");
-const config = require("./config");
-const router = express.Router();
-
-const app = express();
+const bp = require("body-parser");
+var _= require('underscore')
+const mongoose= require('mongoose')
+const empcrud=require('./model/model')
+var app = express();
+var middleware= require('./middleware')
+app.use(express.static("public"));
 app.use(bp.json());
+uid = 1;
+userdata = [];
 
-router.post("/login", (req, res) => {
-  const postdata = req.body;
-  const user = {
-    email: postdata.email,
-    name: postdata.name,
-  };
-  const token = jwt.sign(user, config.secret, { expiresIn: config.tokenLife });
-  const refreshtoken = jwt.sign(user, config.refreshTokenSecret, {
-    expiresIn: config.refreshTokenLife,
+const uri = "mongodb+srv://amar:amar123@cluster0.rle5i.mongodb.net/capdb?retryWrites=true&w=majority&appName=Cluster0";
+
+
+app.use(middleware.logger)
+
+app.get("/loadusers",middleware.requireAuth, async(req, res) => {
+  const users= await empcrud.find()
+  return res.send(users)
+ });
+app.get("/loaduser/:id", async(req, res) => {
+  const uid = parseInt (req.params.id);
+  const user= await empcrud.findById(uid)
+  return res.send(user)
+  
+   
+});
+app.post("/adduser", (req, res) => {
+   const user=new empcrud({...req.body})
+   user.save().then(()=> res.send('user added'))
+  
+});
+
+app.delete("/deleteuser/:id", (req, res) => {
+    const uid = parseInt (req.params.id);
+     var mtd=_.findWhere(userdata,{id:uid})
+     if (mtd) {
+      userdata=_.without(userdata,mtd)
+      res.json(mtd)
+    } else {
+      res.status(404).send();
+    }
   });
 
-  const response = {
-    status: "Logged IN",
-    token: token,
-    refreshToken: refreshtoken,
-  };
-  res.status(200).json(response);
-});
-router.post("/token", (req, res) => {
-  const postData = req.body;
-  if (postData.refreshToken && postData.refreshToken in tokenList) {
-    const user = {
-      email: postData.email,
-      name: postData.name,
-    };
-    const token = jwt.sign(user, config.secret, {
-      expiresIn: config.tokenLife,
-    });
-    const response = {
-      token: token,
-    };
-    tokenList[postData.refreshToken].token = token;
-    res.status(200).json(response);
-  } else {
-    res.status(404).send("invalid request");
-  }
-});
+const startServer=async()=>{
+  await mongoose.connect(uri)
+  app.listen(4000, () => {
+    console.log("server is ready...!");
+  });
+}
 
-router.use(require("./tokenChecker"));
-router.get("/secure", (req, res) => {
-  res.send("I am secured...!");
-});
-app.use("/api", router);
-app.listen(4000, () => {
-  console.log("server is ready");
-});
+startServer()
+
+
