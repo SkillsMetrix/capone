6161c87599a63a5c84d0f0475479f732f2faad97
skillsmetrix Technologies <161203473+SkillsMetrix@users.noreply.github.com>
2024-03-09 05:34:20 +0530

    Update data.js

--- data.js
+++ data.js
@@ -1,43 +1,55 @@
-const jwt= require('jsonwebtoken')
-const config= require('./config')
+const express = require("express");
+var bp = require("body-parser");
+const jwt = require("jsonwebtoken");
+const config = require("./config");
+const router = express.Router();
 
-module.exports=(req,res,next)=>{
-    const token=req.body.token || req.query.token || req.headers['x-access-token']
-    //decode token
-    if(token){
-        jwt.verify(token,config.secret,function(err,decoded){
-            if(err){
-                return res.status(401).json({"error":true,"message":"Unauthorized Access"})
-            }
-            req.decoded=decoded
-            next()
-        })
-    }else{
-        res.status(403).send({
-            "error":true,
-            "message":"No Token Found"
-        })
-    }
-}
+const app = express();
+app.use(bp.json());
 
+router.post("/login", (req, res) => {
+  const postdata = req.body;
+  const user = {
+    email: postdata.email,
+    name: postdata.name,
+  };
+  const token = jwt.sign(user, config.secret, { expiresIn: config.tokenLife });
+  const refreshtoken = jwt.sign(user, config.refreshTokenSecret, {
+    expiresIn: config.refreshTokenLife,
+  });
 
+  const response = {
+    status: "Logged IN",
+    token: token,
+    refreshToken: refreshtoken,
+  };
+  res.status(200).json(response);
+});
+router.post("/token", (req, res) => {
+  const postData = req.body;
+  if (postData.refreshToken && postData.refreshToken in tokenList) {
+    const user = {
+      email: postData.email,
+      name: postData.name,
+    };
+    const token = jwt.sign(user, config.secret, {
+      expiresIn: config.tokenLife,
+    });
+    const response = {
+      token: token,
+    };
+    tokenList[postData.refreshToken].token = token;
+    res.status(200).json(response);
+  } else {
+    res.status(404).send("invalid request");
+  }
+});
 
-
-
-
-const express= require('express')
-var bp= require('body-parser')
-const jwt= require('jsonwebtoken')
-const config= require('./config')
-const router= express.Router()
-
-const app= express()
-
-router.use(require('./tokenChecker'))
-router.get('/secure',(req,res)=>{
-    res.send('I am secured...!')
-})
-app.use('/api',router)
-app.listen(4000,()=>{
-    console.log('server is ready');
-})
+router.use(require("./tokenChecker"));
+router.get("/secure", (req, res) => {
+  res.send("I am secured...!");
+});
+app.use("/api", router);
+app.listen(4000, () => {
+  console.log("server is ready");
+});
