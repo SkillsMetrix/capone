71731b4f3b2ff0255893ee76c1258700de0b0897
skillsmetrix Technologies <161203473+SkillsMetrix@users.noreply.github.com>
2024-03-09 21:37:51 +0530

    Update data.js

--- data.js
+++ data.js
@@ -1,55 +1,109 @@
-const express = require("express");
-const bp = require("body-parser");
-var _= require('underscore')
-const mongoose= require('mongoose')
-const empcrud=require('./model/model')
-var app = express();
-var middleware= require('./middleware')
-app.use(express.static("public"));
-app.use(bp.json());
-uid = 1;
-userdata = [];
-
-const uri = "mongodb+srv://amar:amar123@cluster0.rle5i.mongodb.net/capdb?retryWrites=true&w=majority&appName=Cluster0";
-
-
-app.use(middleware.logger)
-
-app.get("/loadusers",middleware.requireAuth, async(req, res) => {
-  const users= await empcrud.find()
-  return res.send(users)
- });
-app.get("/loaduser/:id", async(req, res) => {
-  const uid = parseInt (req.params.id);
-  const user= await empcrud.findById(uid)
-  return res.send(user)
-  
-   
-});
-app.post("/adduser", (req, res) => {
-   const user=new empcrud({...req.body})
-   user.save().then(()=> res.send('user added'))
-  
-});
-
-app.delete("/deleteuser/:id", (req, res) => {
-    const uid = parseInt (req.params.id);
-     var mtd=_.findWhere(userdata,{id:uid})
-     if (mtd) {
-      userdata=_.without(userdata,mtd)
-      res.json(mtd)
-    } else {
-      res.status(404).send();
+const asyncHandler= require('express-async-handler')
+const User= require('../models/userModel')
+const bcrypt= require('bcrypt')
+const registerUser=asyncHandler(async(req,res)=> {
+    const {username,email,password} = req.body
+    if(!username || !email || !password){
+        res.status(400)
+        throw new Error("All Fields are mandatory")
     }
-  });
+    const userAvailable=await User.findOne({email})
+    if(userAvailable){
+        res.status(400)
+        throw new Error("User already registered")
+    }
+    const hashedPassword=await bcrypt.hash(password,10)
+    console.log('Hashed Password ' ,hashedPassword);
+    res.json({message :'user registered'})
+ })
 
-const startServer=async()=>{
-  await mongoose.connect(uri)
-  app.listen(4000, () => {
-    console.log("server is ready...!");
-  });
-}
+const loginUser=asyncHandler((req,res)=> {
+    res.json({message:' Login User'})
+})
 
-startServer()
+const currentUser=asyncHandler((req,res)=> {
+    res.json({message:'Current User Information'})
+})
+
+module.exports={registerUser,loginUser,currentUser}
 
 
+
+
+
+
+-----------
+
+  const mongoose = require("mongoose");
+const connectDB = async () => {
+  try {
+    const connect = await mongoose.connect(process.env.DB_STRING);
+    console.log("DB Connected");
+  } catch (err) {
+    console.log(err);
+  }
+};
+module.exports = connectDB;
+
+
+------------
+
+
+  const mongoose = require("mongoose");
+const userSchema = mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: [true, "Please add username"],
+    },
+    email: {
+      type: String,
+      required: [true, "Please add email"],
+      unique: [true, "Email is already taken"],
+    },
+    password: {
+      type: String,
+      required: [true, "Please add password"],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+module.exports = mongoose.model("User", userSchema);
+
+
+
+
+-----------
+
+
+  
+const express= require('express')
+const { registerUser, loginUser, currentUser } = require('../controllers/userController')
+const router= express.Router()
+
+router.post('/register',registerUser)
+
+router.post('/login',loginUser)
+
+router.get('/current',currentUser)
+
+module.exports=router
+
+
+---------------
+
+  const express= require('express')
+const connectDB= require('./config/dbConnection')
+const dotenv= require('dotenv').config()
+
+connectDB()
+const app= express()
+
+
+app.use(express.json())
+app.use('/api/users',require('./routes/userRoutes'))
+app.listen(4000,()=>{
+    console.log('server is ready');
+})
