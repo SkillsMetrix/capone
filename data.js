9c23cd560a577ec752c62b9c661ff300e8a693f4
skillsmetrix Technologies <161203473+SkillsMetrix@users.noreply.github.com>
2024-03-14 04:07:29 +0530

    Update data.js

--- data.js
+++ data.js
@@ -1,14 +1,46 @@
- 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <form id="myform">
+
+   <input type="text" name="message">
+   <button>Send</button>
+
+    </form>
+
+
+
+    <script src="js/jquery-2.1.4.min.js"></script>
+    <script src="js/socket.io-1.3.7.js"></script>
+    <script src="js/app.js"></script>
+</body>
+</html>
+
+
+
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static('public'));
 
-io.on('connection', function () {
+io.on('connection', function (socket) {
 	console.log('User connected via socket.io!');
+
+	socket.on('message',function(message){
+		console.log('Message Recieved ' + message.text);
+		socket.broadcast.emit('message',message)
+	})
+	socket.emit('message', {text:'Welcome to chat application'})
 });
+
 
 http.listen(4000, function () {
 	console.log('Server started!');
@@ -17,9 +49,12 @@ http.listen(4000, function () {
 
 
 
--------------------
 
-var socket=io()
+
+--------------
+
+
+	var socket=io()
 
 socket.on('connect',function(){
     console.log('connect to server');
@@ -29,20 +64,16 @@ socket.on('message',function(message){
 console.log('New Message ',message.text);
 })
 
+var $form= jQuery('#myform')
 
+$form.on('submit',function(event){
 
--------
+    event.preventDefault();
 
-	<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <script src="js/jquery-2.1.4.min.js"></script>
-    <script src="js/socket.io-1.3.7.js"></script>
-    <script src="js/app.js"></script>
-</body>
-</html>
+    var $msg= $form.find('input[name=message]')
+
+    socket.emit('message',{
+        text:$msg.val()
+    })
+    $msg.val('')
+})
