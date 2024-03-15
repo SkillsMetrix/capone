c8dbaf3f1a16473093ae31ecf62c4a7861bc4673
skillsmetrix Technologies <161203473+SkillsMetrix@users.noreply.github.com>
2024-03-16 04:23:18 +0530

    Update data.js

--- data.js
+++ data.js
@@ -1,34 +1,19 @@
-function getUser(userId) {
-    return new Promise((resolve, reject) => {
-        console.log('Get the User Details from DB..!');
-        setTimeout(() => {
-            resolve({
-                userId: userId,
-                userName: 'santosh'
-            })
-        }, 1000);
+const p1 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        console.log('The first promise has resolved');
+        resolve(10);
+    }, 1 * 1000);
 
-    })
-}
-function getServicesUsed(user) {
-    return new Promise((resolve, reject) => {
-        console.log(`Get the services of ${user.userName} from the cloud`);
-        setTimeout(() => {
-            resolve(['EC2,VPN,CloudShell'])
-        }, 3 * 1000);
-
-    })
-}
-function getServiceCost(services) {
-    return new Promise((resolve, reject) => {
-        console.log(`Calculate the cost of Services Used ...${services}`);
-        setTimeout(() => {
-            resolve(services.length * 100)
-        }, 2 * 1000);
-
-    })
-}
-getUser(101)
-.then(getServicesUsed)
-.then(getServiceCost)
-.then(console.log)
+});
+const p2 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        console.log('The second promise has rejected');
+        reject('Failed');
+    }, 2 * 1000);
+});
+const p3 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        console.log('The third promise has resolved');
+        resolve(30);
+    }, 3 * 1000);
+});
