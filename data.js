aa5da27817863599e74831d6c1e870fbc2bffdf0
skillsmetrix Technologies <161203473+SkillsMetrix@users.noreply.github.com>
2024-03-20 05:55:54 +0530

    Update data.js

--- data.js
+++ data.js
@@ -1,28 +1,43 @@
-import React, { Component } from "react";
-import axios from "axios";
 
-const URL = "https://jsonplaceholder.typicode.com/users";
-export default class RestApp extends Component {
-  state = {
-    userdata: [],
-  };
 
-  componentDidMount() {
-    axios
-      .get(URL)
-      .then((res) => res.data)
-      .then((data) => {
-        this.setState({ userdata: data });
-      });
-  }
 
-  render() {
-    return <div>
-        {this.state.userdata.map((user)=>(
-            <div key={user.email}>
-                {user.username} -- {user.email}
+import React, { useState } from 'react';
+
+function UsersApp(props) {
+    const [users,setUsers]= useState([])
+    const [uname,setUname]=useState('')
+    const [email,setEmail]=useState('')
+
+    const addUser=(e)=>{
+       e.preventDefault();
+       setUsers([
+            ...users,{uname,email}
+       ])
+       setUname('')
+       setEmail('')
+    }
+    const deleteUser=(user)=>{
+ setUsers(users.filter((note) => note.uname !== user))
+    }
+     return (
+        <div>
+            <div>
+                {users.map((data) => (
+                    <div>{data.uname} --- {data.email}
+                     <button onClick={() => deleteUser(data.uname)}>Delete</button>
+                    </div>
+                  
+                ))}
+               
+                <hr/>
+                <form onSubmit={addUser}>
+                    <input type='text' value={uname} onChange={(e)=> setUname(e.target.value)}/>
+                    <input type='email' value={email} onChange={(e)=> setEmail(e.target.value)}/>
+                    <button>Add User</button>
+                </form>
             </div>
-        ))}
-    </div>;
-  }
+        </div>
+    );
 }
+
+export default UsersApp;
