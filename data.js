fb75cabe9fa531778baee54481ce3ab203b75613
skillsmetrix Technologies <161203473+SkillsMetrix@users.noreply.github.com>
2024-03-23 20:59:59 +0530

    Update data.js

--- data.js
+++ data.js
@@ -5,3 +5,63 @@ import {createStore,applyMiddleware,compose} from 'redux'
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 const appStore= createStore(rootReducers, composeEnhancers(applyMiddleware(thunk)) )
 export default appStore
+
+
+----------
+
+  import apiCall from "../../apis/apiCall"
+import { ActionTypes } from "../constants/action-types"
+
+
+export const setProducts=(product) =>{
+    return {
+        type:ActionTypes.SET_PRODUCTS,
+        payload:product
+    }
+}
+
+export const fetchProducts=() =>async(dispatch) =>{
+    const response= await apiCall.get('/products')
+    dispatch({type:ActionTypes.FETCH_PRODUCTS,payload:response.data})
+}
+
+export const selectedProducts=(product) =>{
+    return {
+        type:ActionTypes.SELECTED_PRODUCT,
+        payload:product
+    }
+}
+------------
+
+
+ import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import axios from "axios";
+
+import ProductComponent from "./ProductComponent";
+import { fetchProducts, setProducts } from "../redux/actions/productActions";
+const URL = "https://fakestoreapi.com/products";
+
+function ProductListing(props) {
+  const dispatch=useDispatch()
+
+
+
+  useEffect(() => {
+    dispatch(fetchProducts())
+  }, []);
+  const products = useSelector((state) => state);
+  console.log(products);
+  return (
+    <div className="ui grid container">
+      <ProductComponent />
+    </div>
+  );
+}
+
+export default ProductListing;
+
+
+------------
+
+ 
